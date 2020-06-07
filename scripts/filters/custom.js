a33745ab66d9a806e3f9d86115af5bf30d337c75
Lin Jiexin <798832135@qq.com>
2020-06-07 13:24:26 +0800

    config: 插入点击出现爱心脚本

--- scripts/filters/custom.js
+++ scripts/filters/custom.js
@@ -1,3 +1,7 @@
 hexo.extend.filter.register('theme_inject', function(injects) {
   injects.style.push('source/_data/custom.styl');
 });
+
+hexo.extend.filter.register('theme_inject', function(injects) {
+  injects.bodyEnd.raw('load-custom-js', '<script src="../../scripts/love.js"></script>', {}, {cache: true});
+});
